911110a2c864856eebe947d633f18620a8e8b0e5
Subert81 <66858697+Subert81@users.noreply.github.com>
2021-05-01 16:01:21 -0500

    Update and rename StevenUbertPoject06.cpp to "Name Goes Here!"Poject06.cpp

--- Project6/"Name Goes Here!"Poject06.cpp
+++ Project6/"Name Goes Here!"Poject06.cpp
@@ -5,13 +5,13 @@
 * code for their implementation, and a main function. 
 * put the code into appropriate files with the necessary include statements 
 * and preprocessor statements so that the program compiles and runs. 
-* It should output ìCircle has radius 2 and area 12.5664î.
+* It should output ‚ÄúCircle has radius 2 and area 12.5664‚Äù.
 * Add another class, Rectangle, that is also derived from the Shape class. modify the Rectangle class appropriately so it has private width and height variables, a constructor that allows the user to set the width and height, functions to retrieve the width and height, and an appropriately defined getArea function that calculates the area of the rectangle.
 *
 * The following code added to main should output 
-* ìrectangle has width 3 has height 4 and area 12.0î.
+* ‚Äúrectangle has width 3 has height 4 and area 12.0‚Äù.
 * 
-* Author Steven Ubert
+* Author "Name Goes Here!"
 * Version: 4/3/2021
 * Pg 884 Practice Program # 3
 */
